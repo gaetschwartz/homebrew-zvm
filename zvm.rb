fc20dd23aafd39915a1fc5247df33649040f6cc7
Gaëtan <blaxoujunior@gmail.com>
2023-02-10 23:04:39 +0100

    fix

--- zvm.rb
+++ zvm.rb
@@ -1,12 +1,10 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
 class Zvm < Formula
-  desc ""
+  desc "Simple yet powerful version manager for Zig"
   homepage "https://github.com/gaetschwartz/zvm"
   url "https://github.com/gaetschwartz/zvm/releases/download/0.0.1-staging.13/zvm-aarch64-macos"
+  version "0.0.1-staging.13"
   sha256 "d6488a716d1d3de70a0d547557c5f75e9146eaa4618c953d4024d63d17257519"
   license ""
-  revision 1
 
   def install
     bin.install "zvm-aarch64-macos" => "zvm"
