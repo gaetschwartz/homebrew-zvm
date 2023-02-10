eac68769e6f576583eea6e2f61647ac17df80f80
Gaëtan <blaxoujunior@gmail.com>
2023-02-10 22:21:40 +0100

    zvm: revision bump

--- zvm.rb
+++ zvm.rb
@@ -6,6 +6,7 @@ class Zvm < Formula
   url "https://github.com/gaetschwartz/zvm/releases/download/0.0.1-staging.13/zvm-aarch64-macos"
   sha256 "d6488a716d1d3de70a0d547557c5f75e9146eaa4618c953d4024d63d17257519"
   license ""
+  revision 1
 
   def install
     bin.install "zvm-aarch64-macos" => "zvm"
